f09c7f416e5446fe4e520b8d4a293bc865b560e6
Igwesi Emmanuel <51293030+igwe9@users.noreply.github.com>
2021-08-15 19:05:55 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,7 @@ def edit(request, slug):
 	if request.POST:
 		form = UpdateBlogForm(request.POST or None, request.FILES or None, instance=blog)
 		if form.is_valid():
-			instance = form.save(commit=false)
+			instance = form.save(commit=False)
 			instance.image = form.image
 			instance.save()
 			return redirect('detail', slug=instance.slug)
